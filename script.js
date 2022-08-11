58fe4d95d6da15aa7158aed3b0af95028865c826
kirtidhawan102 <kirti0388.be20@chitkara.edu.in>
2022-08-11 15:47:13 +0530

    seven commit

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ document.querySelector("#BLOG").addEventListener('click',()=>{
         if(xhr.readyState===4 && xhr.status===200){
            const response=JSON.parse(xhr.responseText);
            console.log(response);
+          
+        
            let output='';
 
            for(let i=0;i<response.length;i++){
+            
             output+=`
            
             <div style="width:300px;margin:20px;margin:0.5em;color:white;box-sizing:border-box;border:0.5px solid white;" class="box">
+            <img src="r15.jpg" style="width:100%">
             <h4 style="font-familt:sans-serif;margin-left:2px">Title:${response[i].title}</h4>
             <p style="color:grey;margin-left:3px">UserId:${response[i].userId}</p>
             <p style="color:grey;margin-left:3px">id:${response[i].id}</p>
